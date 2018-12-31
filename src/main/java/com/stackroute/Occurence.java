fa77980a1036b5761750e7ab0b24918e09aa1ebb
razeej21 <razeej21@in.ibm.com>
2018-12-31 16:32:59 +0530

    Added 1

---
+++ src/main/java/com/stackroute/Occurence.java
@@ -0,0 +1,18 @@
+package com.stackroute;
+
+import java.util.*;
+
+public class Occurence
+{
+    public int count(String s) {
+
+            String str = s.toLowerCase();
+            System.out.println("Enter a character to count in the string " + s);
+            char c = s.toLowerCase().charAt(0); // This logic is to read
+            // only one character
+            int cn = s.length() - s.replaceAll(String.valueOf(c), "").length();
+            System.out.println(c + " occurs " + cn + " times in " + s);
+            return cn;
+        }
+    }
+
