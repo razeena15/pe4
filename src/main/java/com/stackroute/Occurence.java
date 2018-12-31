521d186b97205469ff5a275febd1b264939f7639
pricella10 <chintusonu55@gmail.com>
2018-12-31 18:22:34 +0530

    pricella commit

--- src/main/java/com/stackroute/Occurence.java
+++ src/main/java/com/stackroute/Occurence.java
@@ -2,17 +2,17 @@ package com.stackroute;
 
 import java.util.*;
 
+
 public class Occurence
 {
-    public int count(String s) {
+   public int count(String s,char c) {
 
             String str = s.toLowerCase();
-            System.out.println("Enter a character to count in the string " + s);
-            char c = s.toLowerCase().charAt(0); // This logic is to read
-            // only one character
-            int cn = s.length() - s.replaceAll(String.valueOf(c), "").length();
-            System.out.println(c + " occurs " + cn + " times in " + s);
+            c = str.toLowerCase().charAt(0);
+            int cn = str.length() - str.replaceAll(String.valueOf(c), "").length();
+            System.out.println(c + " occurs " + cn + " times in:\n " + str);
             return cn;
         }
+
     }
 
